3cfa53a2a9f49dc49040c96ebc518e9e5ab6b7ec
Christabel Sebastian <christabel.escarez@gmail.com>
2020-09-15 20:08:10 -0700

    Wrote the first two tests and code to pass.

--- lib/blackjack_score.rb
+++ lib/blackjack_score.rb
@@ -4,4 +4,16 @@ VALID_CARDS = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace']
 
 def blackjack_score(hand)
 
+  score = hand.reduce(0) do |sum, card|
+   if card.class == Integer
+     sum + card
+   else
+      case card
+      when "Jack", "Queen", "King"
+        sum + 10
+      end
+    end
+  end
+
+  return score
 end
--- test/blackjack_score_test.rb
+++ test/blackjack_score_test.rb
@@ -10,19 +10,27 @@ Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 describe 'Blackjack Score' do
   it 'can calculate the score for a pair of number cards' do
-
     # Arrange
     hand = [3, 4]
-
     # Act
     score = blackjack_score(hand)
-
     # Assert <-  You do this part!
-
+    expect(score).must_equal 7
   end
 
   it 'facecards have values calculated correctly' do
-
+    # Arrange
+    hand1 = ["Jack", 4]
+    hand2 = ["Jack", "Queen"]
+    hand3 = [2, "King"]
+    # Act
+    hand1_score = blackjack_score(hand1)
+    hand2_score = blackjack_score(hand2)
+    hand3_score = blackjack_score(hand3)
+    # Assert
+    expect(hand1_score).must_equal 14
+    expect(hand2_score).must_equal 20
+    expect(hand3_score).must_equal 12
   end
 
   it 'calculates aces as 11 where it does not go over 21' do
